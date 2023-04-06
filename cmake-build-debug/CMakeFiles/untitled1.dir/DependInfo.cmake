4c0841f7932f03ae232a909770906681774d47ed
Mad-Desktop <kailontop@gmail.com>
2023-04-06 23:43:03 +0200

    22

---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Mad-Desktop/Desktop/Doa/DOA-lab1/Algo.cpp" "CMakeFiles/untitled1.dir/Algo.cpp.obj" "gcc" "CMakeFiles/untitled1.dir/Algo.cpp.obj.d"
+  "C:/Users/Mad-Desktop/Desktop/Doa/DOA-lab1/main.cpp" "CMakeFiles/untitled1.dir/main.cpp.obj" "gcc" "CMakeFiles/untitled1.dir/main.cpp.obj.d"
+  "C:/Users/Mad-Desktop/Desktop/Doa/DOA-lab1/reader.cpp" "CMakeFiles/untitled1.dir/reader.cpp.obj" "gcc" "CMakeFiles/untitled1.dir/reader.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
