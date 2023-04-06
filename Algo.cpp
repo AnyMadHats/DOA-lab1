bf6bfbe66be98804d3e0bcfea3cb18fe6062e1d8
Mad-Desktop <kailontop@gmail.com>
2023-04-06 23:42:56 +0200

    22

--- Algo.cpp
+++ Algo.cpp
@@ -2,9 +2,11 @@
 // Created by kailo on 2023-04-04.
 //
 
+#include <stack>
 #include "Algo.h"
 
-bool Algo::DFS(adjacency_list_t list) {
-    list.first.
+bool Algo::DFS(adjacency_list_t list, int start) {
+    std::stack<meta_t> stack;
+    stack.push(list.first.find(start)) ;
     return false;
 }
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
 
     adjacency_list_t list = parse_file(R"(C:\Users\kailontop\CLionProjects\untitled\edge.txt)");
+    list.first.
 
 
 
